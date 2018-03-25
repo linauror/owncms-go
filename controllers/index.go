fe1507637e4066b65c2df5d9b95ea1b754b9607f
linauror <linauror@gmail.com>
2018-03-25 23:10:14 +0800

    03.25

--- controllers/index.go
+++ controllers/index.go
@@ -10,7 +10,13 @@ type IndexController struct {
 
 func (c *IndexController) Index() {
 	filter := make(map[string]string)
-	postLists, postTotal := models.PostLists(1, 10, filter)
+
+	title := c.GetString("title")
+	if len(title) > 0 {
+		filter["title"] = title
+	}
+
+	postLists, postTotal := models.PostLists(1, 3, filter)
 
 	c.Data["postTotal"] = postTotal
 	c.Data["postLists"] = postLists
--- models/post.go
+++ models/post.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"strings"
 	"time"
 
 	"github.com/astaxie/beego/orm"
@@ -8,7 +9,7 @@ import (
 
 type Post struct {
 	Id            int       `orm:"column(id)"`
-	Category      uint8     `orm:"column(category)" description:"类别ID"`
+	CategoryId    uint8     `orm:"column(category)" description:"类别ID"`
 	Flag          string    `orm:"column(flag);size(50)" description:"属性"`
 	Litpic        string    `orm:"column(litpic);size(100)" description:"略缩图"`
 	Title         string    `orm:"column(title);size(100)" description:"标题"`
@@ -27,6 +28,7 @@ type Post struct {
 	Template      string    `orm:"column(template);size(50)" description:"模板名称"`
 	Tag           string    `orm:"column(tag);size(100)" description:"标签ID集合"`
 	Ishidden      int8      `orm:"column(ishidden)" description:"是否隐藏"`
+	Channeltype   string
 }
 
 func (t *Post) TableName() string {
@@ -39,23 +41,56 @@ func init() {
 
 func PostLists(page int, pageSize int, filter map[string]string) ([]*Post, int64) {
 	offset := (page - 1) * pageSize
-	query := orm.NewOrm().QueryTable(new(Post))
+	qb, _ := orm.NewQueryBuilder("mysql")
+	list := make([]*Post, 0)
+	qb.Select("post.*", "category.channeltype").From("post").LeftJoin("category").On("post.category = category.id")
+
+	var where []string
+	var cond []string
 	if len(filter) > 0 {
 		for k, v := range filter {
 			switch k {
 			case "title":
-				query.Filter("title__contains", v)
+				where = append(where, "post.title like ?")
+				cond = append(cond, "%"+v+"%")
 				break
 			default:
-				query.Filter(k, v)
+				where = append(where, "post."+k+" = ?")
+				cond = append(cond, v)
 				break
 			}
 		}
 	}
 
-	total, _ := query.Count()
-	list := make([]*Post, 0)
-	query.OrderBy("-sortrank", "-id").Limit(pageSize, offset).All(&list)
+	if len(where) > 0 {
+		qb.Where(strings.Join(where, " AND "))
+	}
 
-	return list, total
+	qb.OrderBy("post.id").Desc().Limit(pageSize).Offset(offset)
+
+	sql := qb.String()
+	o := orm.NewOrm()
+	o.Raw(sql, cond).QueryRows(&list)
+
+	/*
+		query := orm.NewOrm().QueryTable(new(Post))
+		if len(filter) > 0 {
+			for k, v := range filter {
+				switch k {
+				case "title":
+					query.Filter("title__contains", v)
+					break
+				default:
+					query.Filter(k, v)
+					break
+				}
+			}
+		}
+
+		total, _ := query.Count()
+		list := make([]*Post, 0)
+		query.OrderBy("-sortrank", "-id").Limit(pageSize, offset).All(&list)
+	*/
+
+	return list, 10
 }
